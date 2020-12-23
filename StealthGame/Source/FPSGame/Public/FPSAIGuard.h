ea155bcc5ac86bd5548cc7bd61b39ca00e0fda5f
Yong Ju Kwak <amese1225@gmail.com>
2020-12-23 17:58:59 +0900

    ai가 처다본 뒤, 타이머가 지나면 원래대로 돌아가는 설정

--- StealthGame/Source/FPSGame/Public/FPSAIGuard.h
+++ StealthGame/Source/FPSGame/Public/FPSAIGuard.h
@@ -30,6 +30,13 @@ protected:
 	UFUNCTION()
 	void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volumn);
 
+	FRotator OriginalRotation;
+
+	UFUNCTION()
+	void ResetOrientation();
+
+	FTimerHandle TimerHandle_ResetOrientation;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
